86eea8c5bf14a54fc961392038544658c714289c
karla <k.rbandala@gmail.com>
2019-09-21 12:25:37 -0500

    View Authentication with redirection working with test data

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import Login from './components/Login/Login';
+import Dashboard from './components/AdminDashboard/Dashboard';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <BrowserRouter>
+        <Switch>
+          <Route  path="/"
+                  component={Login}
+                  exact />
+
+          <Route  path="/home"
+                  component={Dashboard}
+                  exact />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import { Form, Button } from 'react-bootstrap';
+import { Redirect } from 'react-router-dom';
+import { AUTHENTICATED, ADMIN } from '../../constants/sessionstorage';
 import { limiter, loginContainer, loginWrapper, loginTitle, 
         inputWrapper, buttonWrapper, button100, 
         button100Wrapper, inputStyle } from '../../assets/jss/components/loginStyle';
 
+
+const testData = {
+    email: '12@12.com',
+    pass: '12'
+};
+
 class Login extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            redirect: false
+        };
+
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.onLoginClick = this.onLoginClick.bind(this);
+    }
+
     render() {
+
+        if(this.state.redirect) return <Redirect to='/home'/>
+
         return(
             <div style={limiter}>
                 <div style={loginContainer}>
@@ -17,17 +42,23 @@ class Login extends React.Component {
 
                             <div style={inputWrapper}>
                                 <Form.Label>Correo electrónico</Form.Label>
-                                <Form.Control type="email" style={inputStyle} placeholder="Ingrese su correo"/>
+                                <Form.Control   type="email" 
+                                                style={inputStyle} 
+                                                placeholder="Ingrese su correo"
+                                                onChange={this.handleEmailChange}/>
                             </div>
 
                             <div style={inputWrapper}>
                                 <Form.Label>Contraseña</Form.Label>
-                                <Form.Control type="password" style={inputStyle} placeholder="Ingrese su contraseña"></Form.Control>
+                                <Form.Control   type="password" 
+                                                style={inputStyle} 
+                                                placeholder="Ingrese su contraseña"
+                                                onChange={this.handlePasswordChange}/>
                             </div>
                             
                             <div style={buttonWrapper}>
                                 <div style={button100Wrapper}>
-                                    <Button style={button100}>
+                                    <Button style={button100} onClick={this.onLoginClick}>
                                         Iniciar sesión
                                     </Button>
                                 </div>
@@ -37,6 +68,33 @@ class Login extends React.Component {
                 </div>
             </div>
         );
+    }
+
+
+    //      INPUT HANDLING FUNCTIONS
+
+    handleEmailChange(event) {
+        this.setState({
+            email: event.target.value
+        });
+    }
+
+    handlePasswordChange(event) {
+        this.setState({
+            password: event.target.value
+        });
+    }
+
+    onLoginClick() {
+        if( this.state.email === testData.email &&
+            this.state.password === testData.pass) {
+                sessionStorage.setItem(AUTHENTICATED, true);
+                sessionStorage.setItem(ADMIN, true);
+
+                this.setState({
+                    redirect: true
+                });
+        }
     }
 
 }
