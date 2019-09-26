22b8ac60415e9c10e70a407c0dd4cf904186f6b8
karla <k.rbandala@gmail.com>
2019-09-25 23:22:04 -0500

    Added missing documentation

--- server/controller/UserController.js
+++ server/controller/UserController.js
@@ -2,6 +2,13 @@ const User = require('../models/User');
 
 var UserController = {};
 
+/**
+ *  Function that performs a password login by finding the record by it's email
+ *  and validating that the password matches the record
+ *  @param {String} email Email of the record to look
+ *  @param {String} password Password of the record
+ *  @param {Function} callback Function to perform after record has (or not) been found
+ */
 UserController.passwordLogin = function(email, password, callback) {
     User.findByEmail(email)
      .then(function(record) {
