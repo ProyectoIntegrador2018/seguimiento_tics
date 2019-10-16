6695be92dd64fe4d3de2bb4322a13edd67949a09
Karla <k.rbandala@gmail.com>
2019-10-16 13:22:38 -0500

    Fixed files so that repo can be deployed to heroku

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 
 const port = process.env.PORT || 3001;
 const database =
@@ -21,14 +22,23 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Initial Configuration
-require("./config/admin");
+require("./server/config/admin");
 
 // Routers
-const authRouter = require('./routes/auth');
-const apiRouter = require('./routes/adminapi');
+const authRouter = require('./server/routes/auth');
+const apiRouter = require('./server/routes/adminapi');
 
 app.use('/auth', authRouter);
 app.use('/admin', apiRouter);
+
+// Front-end production configuration
+if(process.env.NODE_ENV === 'production'){
+    // Static folder
+    app.use(express.static('client/build'));
+    app.get('*', function(req, res) {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 app.listen(port);
 console.log(`App listening on port ${port}`);
