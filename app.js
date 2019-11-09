9750b11431a81b313afd619eae884b0bcbb08530
karla <k.rbandala@gmail.com>
2019-11-09 14:56:09 -0600

    scripts

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Initial Configuration
-require("./server/config/admin");
+app.use(express.static('./server/config/admin'))
 
 // Routers
 const authRouter = require('./server/routes/auth');
