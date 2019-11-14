c8d7f8c9f84f2270efd01504dab1dea555bf6370
Karla <k.rbandala@gmail.com>
2019-11-14 11:11:31 -0600

    Added user middleware

--- server/routes/userapi.js
+++ server/routes/userapi.js
@@ -1,18 +1,20 @@
 const express = require('express');
+cosnt umw = require('../middleware/UserMiddleware');
 const UserController = require('../controller/UserController');
 
 const router = express.Router();
 
 /**
  *  Route for fetching the questions of an event
+ * @implements {UserMiddleware} Middleware to make sure the user is the one making the request
  * @param {Object} req Request of get with the id of the event stored in its parameters
  * @param {Object} res Response of get
  */
-router.get('/questions/:id', function(req, res){
+router.get('/questions/:id', umw, function(req, res){
     var event = req.params.id;
     UserController.fetchEventQuestions(event, function(questions) {
         res.send(questions);
     });
-})
+});
 
 module.exports = router;
