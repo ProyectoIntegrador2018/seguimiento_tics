36e5dfa006d96437f08b70f89c77280199cc7a53
Karla <k.rbandala@gmail.com>
2019-11-14 12:15:21 -0600

    Backend now creates and sends the csv template for each event. Also added the created csvs to git ignore

--- server/controller/UserController.js
+++ server/controller/UserController.js
@@ -2,6 +2,8 @@ const User = require('../models/User');
 const Event = require('../models/Event')
 const Question = require('../models/Question');
 
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+
 var UserController = {};
 
 /**
@@ -35,7 +37,6 @@ UserController.passwordLogin = function(email, password, callback) {
  *  @param {Function} callback Function to perform after records have been fetched
  */
 UserController.fetchEventQuestions = function(eventId, callback) {
-  console.log(typeof(eventId));
   Event.fetchQuestions(eventId)
    .then(function(eventDocuments) {
     var questionsIds = eventDocuments[0].questions;
@@ -47,6 +48,67 @@ UserController.fetchEventQuestions = function(eventId, callback) {
    .catch(function(error) {
      console.log(error);
    });
+}
+
+/**
+ *  Function that creates a CSV template with the required, and non required, questions of an event
+ *  @param {String} eventId Id corresponding to the event whose template we are trying to create
+ *  @param {Function} callback Function to perform after the template has been created
+ */
+UserController.createCSVTemplate = function(eventId, callback) {
+  this.fetchEventQuestions(eventId, function(questions) {
+    const storagePath = `./server/public/templates/${eventId}.csv`;
+    var csvHeaders = [];
+    questions = fetchRequiredQuestions().concat(questions);
+    
+    questions.forEach(question => {
+      csvHeaders.push({
+        id: question._id,
+        title: question.text
+      });
+    });
+
+    const csvwriter = createCsvWriter({
+      path: storagePath,
+      header: csvHeaders
+    });
+
+    csvwriter
+     .writeRecords([])
+     .then(function() {
+       console.log('CSV file written successfully');
+       callback();
+     })
+     .catch(function(error) {
+      console.log('ERROR with CSV file write');
+       console.log(error);
+     })
+
+  })
+};
+
+
+/**
+ * ************************************************************************
+ *                              AUXILIAR FUNCTIONS
+ * ************************************************************************
+ */
+
+ /**
+  * Function that creates an array, similar to a Question object, with all the required questions
+  */
+fetchRequiredQuestions = function() {
+  var questions = ['Nombre(s)', 'Apellido paterno', 'Apellido materno', 'Fecha de nacimiento', 'Lugar de nacimiento', 'Sexo', 'Email'];
+  var response = [];
+
+  questions.forEach(question => {
+    response.push({
+      _id: question,
+      text: question
+    })
+  });
+
+  return response;
 }
 
 module.exports = UserController;
--- server/routes/userapi.js
+++ server/routes/userapi.js
@@ -2,6 +2,7 @@ const express = require('express');
 const umw = require('../middleware/UserMiddleware');
 const UserController = require('../controller/UserController');
 
+const path = require("path");
 const router = express.Router();
 
 /**
@@ -10,10 +11,24 @@ const router = express.Router();
  * @param {Object} req Request of get with the id of the event stored in its parameters
  * @param {Object} res Response of get
  */
-router.get('/questions/:id', umw, function(req, res){
+router.get('/questions/:id', umw, function(req, res) {
     var event = req.params.id;
     UserController.fetchEventQuestions(event, function(questions) {
         res.send(questions);
+    });
+});
+
+/**
+ * Route that fetches the CSV template of an event
+ * @implements {UserMiddleware} Middleware to make sure the user is the one making the request
+ * @param {Object} req Request of get with the id of the event stored in its parameters
+ * @param {Object} res Response of get
+ */
+router.get('/csv-template/:id', function(req, res) {
+    var event = req.params.id;
+    UserController.createCSVTemplate(event, function() {
+        const csvpath = path.join(__dirname, `../public/templates/${event}.csv`);
+        res.sendFile(csvpath);
     });
 });
 
