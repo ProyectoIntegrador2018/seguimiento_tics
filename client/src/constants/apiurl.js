d3dd1ffa4913b828e431ea691fdb0cecf178229b
karla <k.rbandala@gmail.com>
2019-11-18 23:54:43 -0600

    Dynamic url

--- client/src/constants/apiurl.js
+++ client/src/constants/apiurl.js
@@ -1,3 +1,5 @@
-const API_URL = "http://localhost:3001";
+const API_URL = process.env.NODE_ENV === 'production' ? 
+                    process.env.APP_URL : 
+                    "http://localhost:3001";
 
 export { API_URL };
