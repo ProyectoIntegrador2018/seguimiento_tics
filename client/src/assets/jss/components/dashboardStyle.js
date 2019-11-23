b0a7a281d817741aa0ed2ac0e620ff810e4edefc
karla <k.rbandala@gmail.com>
2019-11-22 22:27:30 -0600

    Added dashboard styling and data

---
+++ client/src/assets/jss/components/dashboardStyle.js
@@ -0,0 +1,36 @@
+import "./../../fonts/fonts.css";
+
+const mainHeader = {
+    display: "block",
+    fontFamily: "Poppins",
+    fontSize: "39px",
+    fontWeight: "700",
+    color: "#FF5A5F",
+    textAlign: "center"
+}
+const secondHeader = {
+    display: "block",
+    fontFamily: "Poppins",
+    fontSize: "30px",
+    lineHeight: "200px",
+    fontWeight: "700",
+    color: "#333333",
+    textAlign: "center"
+};
+
+const mainBullets = {
+    textAlign: "center",
+    fontSize: "20px",
+    fontWeight: 500,
+}
+
+const diffColor = {
+    color: "#FF5A5F"
+}
+
+export {
+    secondHeader,
+    mainBullets,
+    diffColor,
+    mainHeader
+};
--- client/src/components/User/Dashboard/UserDashboard.js
+++ client/src/components/User/Dashboard/UserDashboard.js
@@ -1,17 +1,17 @@
 import React from "react";
+import { secondHeader, mainBullets, diffColor, mainHeader } from "../../../assets/jss/components/dashboardStyle";
 
 class UserDashboard extends React.Component {
   render() {
     return (
       <div>
-        <table>
-          <tr>
-            <th>User Dashboard</th>
-          </tr>
-          <tr>
-            <td>Records</td>
-          </tr>
-        </table>
+        <span style={mainHeader}>¡Bienvenido!</span>
+        <span style={secondHeader}>Esta aplicación le permite realizar diferentes opciones:</span>
+        <ul style={mainBullets}>
+          <li style={diffColor}>Registrar estudiantes por CSVs o con una form</li>
+          <li>Buscar a estudiantes</li>
+          <li style={diffColor}>Análizar datos con gráficas</li>
+        </ul>
       </div>
     );
   }
